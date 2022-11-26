b5999725734af74494cf59c0c2a4f227c866e9f2
hs12 <77109954+hyun2371@users.noreply.github.com>
2022-11-26 23:24:59 +0900

    [던전] 플레이어 총 타격입음
    총알 크기 키움

--- Assets/Scenes/Dungeon/Script/FPS_Shoot.cs
+++ Assets/Scenes/Dungeon/Script/FPS_Shoot.cs
@@ -43,8 +43,8 @@ public class FPS_Shoot : MonoBehaviour
 
     GameObject InstantiateBullet()
     {
-        GameObject Shooter = GameObject.FindGameObjectWithTag("MainCamera"); //FPS Player�� shooter
-        Vector3 ClonePos = Shooter.transform.position + Shooter.transform.forward * 2f; //2 ���� �տ��� �Ѿ� ����
+        GameObject Shooter = gameObject; //���� ��ũ��Ʈ�� �Ҵ�Ǵ� object
+        Vector3 ClonePos = Shooter.transform.position + Shooter.transform.forward * 1.4f ; //��ġ ����
         Quaternion CloneRot = Shooter.transform.rotation;  //��� ������ �ٶ󺸰� �ִ���
         GameObject Clone = Instantiate(BulletPrefab, ClonePos, CloneRot);
 
@@ -54,10 +54,10 @@ public class FPS_Shoot : MonoBehaviour
     void InstantiateParticle(GameObject Particle)
     {
         GameObject Shooter = GameObject.FindGameObjectWithTag("MainCamera");
-        Vector3 ClonePos = Shooter.transform.position + Shooter.transform.forward * 0.6f;
+        Vector3 ClonePos = Shooter.transform.position + Shooter.transform.forward * 0.6f + Shooter.transform.up * 1.8f;
         Quaternion CloneRot = Shooter.transform.rotation;
         GameObject Clone = Instantiate(Particle, ClonePos, CloneRot);
-        Clone.transform.localScale = Vector3.one * 0.5f;  // particle ũ�� 0.5���� ����
+        Clone.transform.localScale = Vector3.one * 0.3f;  // particle ũ�� 0.5���� ����
         Destroy(Clone, 2f);
 
 
