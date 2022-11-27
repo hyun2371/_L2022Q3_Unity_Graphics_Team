2317d07fa959a1a4115d70bf5eb42462c5d622c9
hs12 <77109954+hyun2371@users.noreply.github.com>
2022-11-28 02:02:01 +0900

    image effect 추가
    끄고 킬 수 있는 방법을 찾아야 될 듯

---
+++ Assets/Scenes/Dungeon/Script/PostEffect_Blood.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect_Blood : MonoBehaviour
+{
+    Shader myShader;        // image effect shader 
+    Material myMaterial;
+
+    public Texture2D BlendTexture;
+    public float blendOpacity = 1.0f;
+
+    void Start()
+    {
+        myShader = Shader.Find("My/PostEffects/Blood");    // image effect shader file must have been created
+        myMaterial = new Material(myShader);
+    }
+
+    private void Update()
+    {
+        blendOpacity = Mathf.Clamp(blendOpacity, 0.0f, 1.0f);
+    }
+
+    private void OnDisable()
+    {
+        if (myMaterial)
+        {
+            DestroyImmediate(myMaterial);
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        myMaterial.SetTexture("_BlendTex", BlendTexture);
+        myMaterial.SetFloat("_Opacity", blendOpacity);
+        Graphics.Blit(source, destination, myMaterial);
+    }
+}
