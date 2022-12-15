1733e564c750f7813803aff83a33176b1446f060
yejiKim <yeji@beex.team>
2022-12-16 00:46:40 +0900

    지상, 지하 - game view, text 크기 조정

---
+++ Assets/Scenes/City/Script/IndexSceneController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexSceneController : MonoBehaviour
+{
+
+    public GameObject text;
+    public bool isOn;
+
+    void Start()
+    {
+        isOn = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if(isOn)
+            {
+                text.SetActive(false);
+                isOn = false;
+            }
+            else
+            {
+                text.SetActive(true);
+                isOn = true;
+            }
+        }
+    }
+}
