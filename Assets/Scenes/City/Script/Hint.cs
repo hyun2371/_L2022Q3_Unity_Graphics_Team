029343324c54c678d6dd6075aeaf350939029595
yejiKim <yeji@beex.team>
2022-12-18 23:51:52 +0900

    마지막 수정

--- Assets/Scenes/City/Script/Hint.cs
+++ Assets/Scenes/City/Script/Hint.cs
@@ -8,33 +8,21 @@ public class Hint : MonoBehaviour
     public GameObject hint;
     public bool isOn;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if(Input.GetMouseButtonDown(0))
-    //    {
-    //        if (!isOn)
-    //            hint.SetActive(true);
-    //        else
-    //            hint.SetActive(false);
-    //        isOn = !isOn;
-    //    }
-    //}
-
-    public void showHint()
+    void Update()
     {
-        //if (!isOn)
-        //    hint.SetActive(true);
-        //else
-        //    hint.SetActive(false);
-        //isOn = !isOn;
-        hint.SetActive(true);
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isOn)
+                hint.SetActive(true);
+            else
+                hint.SetActive(false);
+            isOn = !isOn;
+        }
     }
+
 }
